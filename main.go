a68becb2d780c2c8824b3a4628d84b574b3a532a
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-08-11 18:04:58 +0530

    Fetching reviews and serializing all products to JSON.

--- main.go
+++ main.go
@@ -9,17 +9,66 @@ import (
 )
 
 type Product struct {
-	Name     string
-	Link     string
-	Image    string
-	Price    string
-	Comments []Comment
+	Name    string
+	Link    string
+	Image   string
+	Price   string
+	Reviews []Review
 }
 
-type Comment struct {
+type Review struct {
 	Name    string
-	Rating  int
+	Rating  string
 	Content string
+}
+
+func (review *Review) parseHtml(raw soup.Root) {
+	review.Name = raw.Find("span", "class", "a-profile-name").Text()
+	review.Rating = raw.Find("span", "class", "a-icon-alt").Text()
+	review.Content = raw.Find("div", "class", "a-expander-content").Text()
+}
+
+func (product *Product) getReviews() {
+	resp, err := soup.Get(product.Link)
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	doc := soup.HTMLParse(resp)
+
+	reviewsContainer := doc.Find("div", "class", "reviews-content")
+
+	if reviewsContainer.Error != nil {
+		return
+	}
+
+	rawReviews := reviewsContainer.FindAll("div", "class", "review")
+	reviews := []Review{}
+
+	for _, rawReview := range rawReviews {
+		contentHolder := rawReview.Find("div", "class", "a-expander-content")
+
+		if contentHolder.Error != nil {
+			continue
+		}
+
+		review := Review{}
+		review.parseHtml(rawReview)
+
+		reviews = append(reviews, review)
+	}
+
+	product.Reviews = reviews
+}
+
+func (product *Product) parseHtml(result soup.Root) {
+	product.Link = result.Find("a", "class", "s-access-detail-page").Attrs()["href"]
+	product.Name = result.Find("h2", "class", "s-access-title").Text()
+	product.Image = result.Find("img", "class", "s-access-image").Attrs()["src"]
+	product.Price = result.Find("span", "class", "s-price").Text()
+
+	product.getReviews()
 }
 
 func main() {
@@ -34,12 +83,9 @@ func main() {
 	now := time.Now().UTC()
 
 	for _, result := range results {
-		link := result.Find("a", "class", "s-access-detail-page").Attrs()["href"]
-		name := result.Find("h2", "class", "s-access-title").Text()
-		image := result.Find("img", "class", "s-access-image").Attrs()["src"]
-		price := result.Find("span", "class", "s-price").Text()
+		product := Product{}
 
-		product := Product{Name: name, Link: link, Image: image, Price: price}
+		product.parseHtml(result)
 
 		json.NewEncoder(os.Stdout).Encode(product)
 	}
